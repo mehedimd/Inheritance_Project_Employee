e4a53f4b0bca167177494d8490d3ac184b224ba4
WADA_58 <WADA_58@DESKTOP-5HQIB3T>
2024-01-31 15:17:02 +0600

    Add project files.

---
+++ Avidence13/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Avidence13
+{
+    internal class Employee : Person
+    {
+        double salary;
+        public double _Salary { get { return salary; } set { salary = value; } }
+        public Employee(double salary, string nm, int ag): base(name: nm, age: ag)
+        {
+            this.salary = salary;
+            
+        }
+
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine($"Person Name: {_Name}, age: {_Age}, salary: {_Salary}"); 
+        }
+        public override void PrintDetails()
+        {
+            Console.WriteLine($"Person Name: {_Name}, age: {_Age}, salary: {_Salary}");
+        }
+
+        static void Main()
+        {
+            Employee e = new Employee(2500,"mehu", 26);
+            e.PrintEmployeeDetails();
+            e.PrintDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Avidence13/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Avidence13
+{
+    internal class Person
+    {
+        string name; int age;
+        static int totalCount = 0;
+        public string _Name { get { return name; } set { name = value; } }
+        public int _Age { get { return age; } set { age = value; } }
+
+
+        //public Person()
+        //{
+        //    ++totalCount;
+        //}
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            totalCount++;
+        }
+        public virtual void PrintDetails()
+        {
+            Console.WriteLine($"Person Name: {name}, age: {age}");
+        }
+
+        public  void  PrintDetails( string customMsg)
+        {
+            Console.WriteLine($"Person Name: {name}, age: {age}, Custom Message: {customMsg}");
+        }
+        public Person(string name = "No Name")
+        {
+            this.name=name;
+        }
+        public Person(int age = 0)
+        {
+            this.age=age;
+        }
+        
+        public string SetName(string newName)
+        {
+            if (newName == null || newName =="" )
+            {
+                throw new ArgumentNullException("name can't be null");
+            }
+            else
+            {
+                return this.name = newName;
+            }
+        }
+
+        static void Main()
+        {
+            Person p1 = new Person("Mehedi Hasan", 22);
+            p1.PrintDetails();
+            Person p2 = new Person("Al-Amin", 29);
+            p2.PrintDetails("All the best!");
+            p2.SetName("Rabbi");
+            p2.PrintDetails();
+            p2.PrintDetails("All the best!");
+
+            Console.WriteLine($"Number of object Created: {totalCount}");
+
+            Person[] person = { p1, p2 };
+            foreach (Person obj in person)
+            {
+                obj.PrintDetails() ;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+    
+}
